bcf51c4aac7194731962493846f8667fcc78379a
ccdiazclaro <46222556@claro.com.co>
2025-04-08 17:48:33 -0500

    Update: se cambia de version a laravel 11

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,14 +6,14 @@
 
     <div class="card shadow p-4" style="width: 100%; max-width: 400px; border-radius: 8px;">
         @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <h1 class="text-center mb-4">Inicia Sesión</h1>
         <form action="/login" method="POST">
             @csrf
@@ -26,9 +26,15 @@
                 @enderror
             </div>
 
-            <div class="form-floating mb-3">
-                <input type="password" name="password" placeholder="Contraseña" class="form-control" id="exampleInputPassword1">
-                <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+            <!-- Campo de contraseña con toggle para visualizar -->
+            <div class="mb-3 position-relative">
+                <div class="form-floating">
+                    <input type="password" name="password" class="form-control" placeholder="Contraseña" id="exampleInputPassword1">
+                    <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+                </div>
+                <button type="button" id="togglePassword" class="btn btn-outline-secondary position-absolute" style="top: 50%; right: 10px; transform: translateY(-50%);">
+                    <i class="fa fa-eye"></i>
+                </button>
                 @error('password')
                     <small class="text-danger">{{ $message }}</small>
                 @enderror
@@ -42,5 +48,18 @@
         </form>
     </div>
 </div>
+
+<script>
+    document.getElementById('togglePassword').addEventListener('click', function () {
+        const passwordInput = document.getElementById('exampleInputPassword1');
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+            this.innerHTML = '<i class="fa fa-eye-slash"></i>';
+        } else {
+            passwordInput.type = 'password';
+            this.innerHTML = '<i class="fa fa-eye"></i>';
+        }
+    });
+</script>
 
 @endsection
--- resources/views/layouts/app-master.blade.php
+++ resources/views/layouts/app-master.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>SDGV</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="{{url('assets/css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
 </head>
